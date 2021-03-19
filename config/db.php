784f2b2716803d63b1261f7fa112db264f6c68c1
admin <admin@example.com>
2021-03-19 11:03:36 +0530

    all files committed at once

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=database-1.cq1kx9ztcx7h.ap-south-1.rds.amazonaws.com;dbname=testdb',
+    'username' => 'admin',
+    'password' => 'testtest',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
